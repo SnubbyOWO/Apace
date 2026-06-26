Earth-Restored/Solace

Address PR feedback and product info lookup

src/Solace.ApiServer/Controllers/EarthApi/CatalogController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text.Json;
 using Solace.ApiServer.Types.Catalog;
 using Solace.ApiServer.Utils;
 using Solace.StaticData;
@@ -47,6 +48,33 @@ internal sealed class CatalogController : SolaceControllerBase
     [HttpGet("products/catalog")]
     public ContentHttpResult GetNFCBoostsCatalog()
         => EarthJson(MakeNFCBoostsCatalogApiResponse(_catalog));
+
+    [HttpGet("products/getProductInfo")]
+    [HttpPost("products/getProductInfo")]
+    public async Task<ContentHttpResult> GetProductInfo(CancellationToken cancellationToken)
+    {
+        HashSet<string> requestedProductIds = ProductIdsFromQuery();
+        if (requestedProductIds.Count == 0 && Request.ContentLength is not null and not 0)
+        {
+            requestedProductIds = await ReadRequestedProductIdsAsync(Request.Body, cancellationToken);
+        }
+
+        NFCBoost[] products = MakeNFCBoostsCatalogApiResponse(_catalog);
+        NFCBoost[] matchingProducts = requestedProductIds.Count == 0
+            ? products
+            : [.. products.Where(product => requestedProductIds.Contains(product.Id))];
+        string[] invalidProductIds = requestedProductIds.Count == 0
+            ? []
+            : [.. requestedProductIds.Except(matchingProducts.Select(product => product.Id))];
+
+        return EarthJson(new Dictionary<string, object>
+        {
+            ["products"] = matchingProducts,
+            ["productInfos"] = matchingProducts,
+            ["recentlyViewedProductIds"] = matchingProducts.Select(product => product.Id).ToArray(),
+            ["invalidProductIds"] = invalidProductIds
+        });
+    }
 
     // TODO: cache these?
     private static ItemsCatalog MakeItemsCatalogApiResponse(Catalog catalog)
@@ -444,4 +472,82 @@ internal sealed class CatalogController : SolaceControllerBase
             miniFig.Deprecated,
             miniFig.ToolsVersion
         ))];
+
+    private HashSet<string> ProductIdsFromQuery()
+    {
+        var productIds = new HashSet<string>(StringComparer.Ordinal);
+        foreach (string key in new[] { "productId", "id", "productIds", "recentlyViewedProductIds", "ids" })
+        {
+            foreach (string? value in Request.Query[key])
+            {
+                if (value is null)
+                {
+                    continue;
+                }
+
+                foreach (string productId in value.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+                {
+                    productIds.Add(productId);
+                }
+            }
+        }
+
+        return productIds;
+    }
+
+    private static async Task<HashSet<string>> ReadRequestedProductIdsAsync(Stream body, CancellationToken cancellationToken)
+    {
+        var productIds = new HashSet<string>(StringComparer.Ordinal);
+
+        try
+        {
+            using JsonDocument document = await JsonDocument.ParseAsync(body, cancellationToken: cancellationToken);
+            AddProductIds(document.RootElement, productIds);
+        }
+        catch (JsonException)
+        {
+        }
+
+        return productIds;
+    }
+
+    private static void AddProductIds(JsonElement element, HashSet<string> productIds)
+    {
+        switch (element.ValueKind)
+        {
+            case JsonValueKind.Object:
+                foreach (JsonProperty property in element.EnumerateObject())
+                {
+                    if (property.Name.Equals("productId", StringComparison.OrdinalIgnoreCase)
+                        || property.Name.Equals("id", StringComparison.OrdinalIgnoreCase)
+                        || property.Name.Equals("productIds", StringComparison.OrdinalIgnoreCase)
+                        || property.Name.Equals("recentlyViewedProductIds", StringComparison.OrdinalIgnoreCase)
+                        || property.Name.Equals("ids", StringComparison.OrdinalIgnoreCase))
+                    {
+                        AddProductIds(property.Value, productIds);
+                    }
+                    else if (property.Value.ValueKind is JsonValueKind.Object or JsonValueKind.Array)
+                    {
+                        AddProductIds(property.Value, productIds);
+                    }
+                }
+
+                break;
+            case JsonValueKind.Array:
+                foreach (JsonElement item in element.EnumerateArray())
+                {
+                    AddProductIds(item, productIds);
+                }
+
+                break;
+            case JsonValueKind.String:
+                string? productId = element.GetString();
+                if (!string.IsNullOrWhiteSpace(productId))
+                {
+                    productIds.Add(productId);
+                }
+
+                break;
+        }
+    }
 }

SnubbyOWO/Apace

src/Solace.ApiServer/Controllers/EarthApi/CatalogController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text.Json;
 using Solace.ApiServer.Types.Catalog;
 using Solace.ApiServer.Utils;
 using Solace.StaticData;
@@ -41,6 +42,33 @@ internal sealed class CatalogController : SolaceControllerBase
     [HttpGet("products/catalog")]
     public ContentHttpResult GetNFCBoostsCatalog()
         => EarthJson(MakeNFCBoostsCatalogApiResponse(catalog));
+
+    [HttpGet("products/getProductInfo")]
+    [HttpPost("products/getProductInfo")]
+    public async Task<ContentHttpResult> GetProductInfo(CancellationToken cancellationToken)
+    {
+        HashSet<string> requestedProductIds = ProductIdsFromQuery();
+        if (requestedProductIds.Count == 0 && Request.ContentLength is not null and not 0)
+        {
+            requestedProductIds = await ReadRequestedProductIdsAsync(Request.Body, cancellationToken);
+        }
+
+        NFCBoost[] products = MakeNFCBoostsCatalogApiResponse(catalog);
+        NFCBoost[] matchingProducts = requestedProductIds.Count == 0
+            ? products
+            : [.. products.Where(product => requestedProductIds.Contains(product.Id))];
+        string[] invalidProductIds = requestedProductIds.Count == 0
+            ? []
+            : [.. requestedProductIds.Except(matchingProducts.Select(product => product.Id))];
+
+        return EarthJson(new Dictionary<string, object>
+        {
+            ["products"] = matchingProducts,
+            ["productInfos"] = matchingProducts,
+            ["recentlyViewedProductIds"] = matchingProducts.Select(product => product.Id).ToArray(),
+            ["invalidProductIds"] = invalidProductIds
+        });
+    }
 
     // TODO: cache these?
     private static ItemsCatalog MakeItemsCatalogApiResponse(Catalog catalog)
@@ -438,4 +466,82 @@ internal sealed class CatalogController : SolaceControllerBase
             miniFig.Deprecated,
             miniFig.ToolsVersion
         ))];
+
+    private HashSet<string> ProductIdsFromQuery()
+    {
+        var productIds = new HashSet<string>(StringComparer.Ordinal);
+        foreach (string key in new[] { "productId", "id", "productIds", "recentlyViewedProductIds", "ids" })
+        {
+            foreach (string? value in Request.Query[key])
+            {
+                if (value is null)
+                {
+                    continue;
+                }
+
+                foreach (string productId in value.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+                {
+                    productIds.Add(productId);
+                }
+            }
+        }
+
+        return productIds;
+    }
+
+    private static async Task<HashSet<string>> ReadRequestedProductIdsAsync(Stream body, CancellationToken cancellationToken)
+    {
+        var productIds = new HashSet<string>(StringComparer.Ordinal);
+
+        try
+        {
+            using JsonDocument document = await JsonDocument.ParseAsync(body, cancellationToken: cancellationToken);
+            AddProductIds(document.RootElement, productIds);
+        }
+        catch (JsonException)
+        {
+        }
+
+        return productIds;
+    }
+
+    private static void AddProductIds(JsonElement element, HashSet<string> productIds)
+    {
+        switch (element.ValueKind)
+        {
+            case JsonValueKind.Object:
+                foreach (JsonProperty property in element.EnumerateObject())
+                {
+                    if (property.Name.Equals("productId", StringComparison.OrdinalIgnoreCase)
+                        || property.Name.Equals("id", StringComparison.OrdinalIgnoreCase)
+                        || property.Name.Equals("productIds", StringComparison.OrdinalIgnoreCase)
+                        || property.Name.Equals("recentlyViewedProductIds", StringComparison.OrdinalIgnoreCase)
+                        || property.Name.Equals("ids", StringComparison.OrdinalIgnoreCase))
+                    {
+                        AddProductIds(property.Value, productIds);
+                    }
+                    else if (property.Value.ValueKind is JsonValueKind.Object or JsonValueKind.Array)
+                    {
+                        AddProductIds(property.Value, productIds);
+                    }
+                }
+
+                break;
+            case JsonValueKind.Array:
+                foreach (JsonElement item in element.EnumerateArray())
+                {
+                    AddProductIds(item, productIds);
+                }
+
+                break;
+            case JsonValueKind.String:
+                string? productId = element.GetString();
+                if (!string.IsNullOrWhiteSpace(productId))
+                {
+                    productIds.Add(productId);
+                }
+
+                break;
+        }
+    }
 }